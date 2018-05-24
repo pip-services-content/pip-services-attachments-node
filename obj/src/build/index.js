f73f14a6fc86e751b8d0a7cfddcdb4be041c9afd
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-24 19:43:21 +0300

    added old obj folder to build facade [skip ci]

---
+++ obj/src/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var AttachmentsServiceFactory_1 = require("./AttachmentsServiceFactory");
+exports.AttachmentsServiceFactory = AttachmentsServiceFactory_1.AttachmentsServiceFactory;
+//# sourceMappingURL=index.js.map
