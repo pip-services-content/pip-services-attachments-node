8efc4344bf7b69896e310e042c6ab4f8fcd3c264
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-24 14:43:14 +0300

    added unsaved changes

--- src/container/AttachmentsProcess.ts
+++ src/container/AttachmentsProcess.ts
@@ -1,5 +1,7 @@
 import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
+import { DefaultNetFactory } from 'pip-services-net-node';
+import { DefaultOssFactory } from 'pip-services-oss-node';
 
 import { AttachmentsServiceFactory } from '../build/AttachmentsServiceFactory';
 
@@ -8,7 +10,8 @@ export class AttachmentsProcess extends ProcessContainer {
     public constructor() {
         super("attachments", "Blob attachments microservice");
         this._factories.add(new AttachmentsServiceFactory);
+        this._factories.add(new DefaultNetFactory);
+        this._factories.add(new DefaultOssFactory);
     }
-
 
 }
