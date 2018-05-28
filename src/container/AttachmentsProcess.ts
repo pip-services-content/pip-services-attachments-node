4912dde9fd4d5a9e5cadc9ce6baefa640130f7e3
Dmitriy Krayniy <krdima92@gmail.com>
2018-05-28 20:04:06 +0300

    added blobs client factory

--- src/container/AttachmentsProcess.ts
+++ src/container/AttachmentsProcess.ts
@@ -3,6 +3,8 @@ import { ProcessContainer } from 'pip-services-container-node';
 import { DefaultNetFactory } from 'pip-services-net-node';
 import { DefaultOssFactory } from 'pip-services-oss-node';
 
+import { BlobsClientFactory } from 'pip-clients-blobs-node';
+
 import { AttachmentsServiceFactory } from '../build/AttachmentsServiceFactory';
 
 export class AttachmentsProcess extends ProcessContainer {
@@ -10,6 +12,7 @@ export class AttachmentsProcess extends ProcessContainer {
     public constructor() {
         super("attachments", "Blob attachments microservice");
         this._factories.add(new AttachmentsServiceFactory);
+        this._factories.add(new BlobsClientFactory);
         this._factories.add(new DefaultNetFactory);
         this._factories.add(new DefaultOssFactory);
     }
