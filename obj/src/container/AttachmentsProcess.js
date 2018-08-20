d76a8f787d9801be4c5bba8af5b66fa4e33e40b7
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:01:28 +0300

    added DefaultRpcFactory

--- obj/src/container/AttachmentsProcess.js
+++ obj/src/container/AttachmentsProcess.js
@@ -3,11 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const pip_clients_blobs_node_1 = require("pip-clients-blobs-node");
 const AttachmentsServiceFactory_1 = require("../build/AttachmentsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class AttachmentsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("attachments", "Blob attachments microservice");
         this._factories.add(new AttachmentsServiceFactory_1.AttachmentsServiceFactory);
         this._factories.add(new pip_clients_blobs_node_1.BlobsClientFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.AttachmentsProcess = AttachmentsProcess;
--- src/container/AttachmentsProcess.ts
+++ src/container/AttachmentsProcess.ts
@@ -2,8 +2,8 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { BlobsClientFactory } from 'pip-clients-blobs-node';
-
 import { AttachmentsServiceFactory } from '../build/AttachmentsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class AttachmentsProcess extends ProcessContainer {
 
@@ -11,6 +11,7 @@ export class AttachmentsProcess extends ProcessContainer {
         super("attachments", "Blob attachments microservice");
         this._factories.add(new AttachmentsServiceFactory);
         this._factories.add(new BlobsClientFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
