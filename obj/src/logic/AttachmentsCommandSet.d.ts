fb3d7544306c9389298a84b7fc14feb7f2eb85dd
pipdeveloper <pipdevs@gmail.com>
2019-05-22 12:37:41 +0000

    project build by Travis CI [skip ci]

--- obj/src/logic/AttachmentsCommandSet.d.ts
+++ obj/src/logic/AttachmentsCommandSet.d.ts
@@ -3,9 +3,9 @@ import { IAttachmentsController } from './IAttachmentsController';
 export declare class AttachmentsCommandSet extends CommandSet {
     private _logic;
     constructor(logic: IAttachmentsController);
-    private makeGetAttachmentByIdCommand;
-    private makeAddAttachmentsCommand;
-    private makeUpdateAttachmentsCommand;
-    private makeRemoveAttachmentsCommand;
-    private makeDeleteAttachmentByIdCommand;
+    private makeGetAttachmentByIdCommand();
+    private makeAddAttachmentsCommand();
+    private makeUpdateAttachmentsCommand();
+    private makeRemoveAttachmentsCommand();
+    private makeDeleteAttachmentByIdCommand();
 }
